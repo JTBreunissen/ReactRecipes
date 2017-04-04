436ba5060c5077aa627d6265869311def0737cb6
JTBreunissen <jantjarco@gmail.com>
2017-04-04 11:46:29 +0200

    added tests

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import Title from './components/Title'
 
 class App extends React.Component {
   render() {
     return (
-      <h1>Hello World!</h1>
+      <div>
+        <Title content="Hello, World" />
+        <Title content="All Recipes" />
+      </div>
     )
   }
 }
